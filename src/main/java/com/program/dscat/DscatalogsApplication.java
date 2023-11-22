fc80d9ee773c383658f3bdc47ac41d564c555d93
Felipe Steffan <felip.developer@hotmail.com>
2023-11-21 22:53:07 -0300

    create project dscatalogs

---
+++ src/main/java/com/program/dscat/DscatalogsApplication.java
@@ -0,0 +1,13 @@
+package com.program.dscat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalogsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/program/dscat/DscatalogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.program.dscat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
